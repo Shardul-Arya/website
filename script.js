5753792540f1ce3a5b99c31ae1cfb1d7762f3aef
Shardul-Arya <74579131+Shardul-Arya@users.noreply.github.com>
2025-07-22 13:23:02 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.carousel-container').forEach(container => {
-  container.addEventListener('wheel', (e) => {
-    e.preventDefault();
-    container.scrollLeft += e.deltaY;
-  });
-});
